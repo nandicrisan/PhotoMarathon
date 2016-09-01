a50e710b39a385ca38074faea89f529111e3fce9
Levi <Levi@LEVID-PC>
2016-09-01 15:07:38 +0300

    General

---
+++ src/PhotoMarathon.Service/Utils/Enum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoMarathon.Service.Utils
+{
+    public class Enum
+    {
+        enum Pages
+        {
+            NotFound = 0,
+            About = 1,
+            Expo = 2,
+            Workshop = 3,
+            Marathon = 4,
+            Register = 5,
+            Blog = 6,
+            Partners = 7,
+            Contact = 8
+        }
+    }
+}
