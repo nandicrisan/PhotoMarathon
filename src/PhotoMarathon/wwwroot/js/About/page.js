69fda60cdcf4067080be40a6d5bbc8f99ef2f293
Crisan <Crisan@CRISAN>
2016-09-06 21:50:26 +0300

    Small changes (carousel remove)

--- src/PhotoMarathon/wwwroot/js/About/page.js
+++ src/PhotoMarathon/wwwroot/js/About/page.js
@@ -1,25 +1,5 @@
 ﻿About = {};
 About.InitCarousel = function () {
-    jQuery(".owl-carousel.owl-services").owlCarousel({
-        items: 4,
-        margin: 30,
-        nav: false,
-        navText: ['', ''],
-        dots: true,
-        loop: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            992: {
-                items: 3
-            }
-        }
-    });
-
     jQuery(".owl-carousel.owl-testimonials2").owlCarousel({
         items: 1,
         nav: false,
