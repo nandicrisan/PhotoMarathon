ae239133ad45fb2d08a49f7a25bafde2041d6239
Levi-laptop <Levi-laptop@DESKTOP-QMMMJ7T>
2016-09-10 17:34:16 +0300

    calendar

--- src/PhotoMarathon/wwwroot/js/Index/Page/index.js
+++ src/PhotoMarathon/wwwroot/js/Index/Page/index.js
@@ -3,19 +3,28 @@
 Main.InitCalendar = function () {
     var currentMonth = moment().format('YYYY-MM');
     var nextMonth = moment().add('month', 1).format('YYYY-MM');
-    var events = [
-      { date: '2016-09-21', title: 'Start înscrieri' },
-      { date: '2016-10-15', title: 'Stabilirea temelor pentru fiecare categorie, competiție' },
-      { date: '2016-10-16', title: 'Jurizare' },
-      { date: '2016-10-17', title: 'Premierea câştigătorilor' },
-      { date: '2016-10-21', title: 'Expoziție Photo Marathon’16' }
+    var lotsOfEvents = [
+        {
+            end: '2016-11-08',
+            start: '2016-12-04',
+            title: 'Monday to Friday Event'
+        }, {
+            end: '2016-11-20',
+            start: '2016-12-15',
+            title: 'Another Long Event'
+        },
+        {
+            date: '2016-09-20',
+            title: 'Another Long Event'
+        }
     ];
 
     $('#mini-clndr').clndr({
         daysOfTheWeek: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
         numberOfRows: 5,
         template: $('#calendar-template').html(),
-        events: events,
+        events: lotsOfEvents,
+        weekOffset: 1,
         clickEvents: {
             click: function (target) {
                 console.log(target);
@@ -27,6 +36,11 @@ Main.InitCalendar = function () {
         doneRendering: function () {
             console.log('this would be a fine place to attach custom event handlers.');
         },
+        dateParameter: 'date',
+        //multiDayEvents: {
+        //    endDate: 'end',
+        //    startDate: 'start'
+        //},
         adjacentDaysChangeMonth: true
     });
 }
